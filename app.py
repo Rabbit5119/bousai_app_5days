abff7d424f515c1392211a27c01815be9966511e
Rabbit5119 <136865696+Rabbit5119@users.noreply.github.com>
2026-09-10 06:36:48 +0000

    あ

--- app.py
+++ app.py
@@ -494,4 +494,4 @@ def api_weather_warnings():
     return jsonify(get_weather_warnings())
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
